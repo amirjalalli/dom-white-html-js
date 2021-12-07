e9daa4729a0d0f9370979ef30194e5734389becd
amirjalalli <87828297+amirjalalli@users.noreply.github.com>
2021-12-07 19:45:15 +0330

    dom-white-html-js

---
+++ js.js
@@ -0,0 +1,18 @@
+let divElem = document.getElementById("getElement") 
+// console.log(divElem)  
+
+let ulElem = document.getElementsByTagName("ul")
+
+// console.log(ulElem)
+
+let liElems = document.getElementsByClassName("items");
+
+// console.log(liElems)
+
+let firstquElem = document.querySelector('.items')
+
+// console.log(firstquElem)
+
+let ulQuElem = document.querySelectorAll('.items')
+
+console.log(ulQuElem)
